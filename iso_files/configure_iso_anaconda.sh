39347d61643042496f46e4dafc69e7b53e37836f
inffy <10782843+inffy@users.noreply.github.com>
2026-01-27 13:11:41 +0200

    chore: cleanup iso script
    This doesn't really affect anything as we don't have these packages anymore, so this is just more of cleanup

--- iso_files/configure_iso_anaconda.sh
+++ iso_files/configure_iso_anaconda.sh
@@ -9,9 +9,6 @@ IMAGE_REF="${IMAGE_REF##*://}"
 sbkey='https://github.com/ublue-os/akmods/raw/main/certs/public_key.der'
 
 # Configure Live Environment
-## Remove packages from liveCD to save space
-dnf remove -y ublue-brew ublue-motd || true
-
 glib-compile-schemas /usr/share/glib-2.0/schemas
 
 systemctl disable rpm-ostree-countme.service
